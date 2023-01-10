cc9292ae9d90ab594101ce47effded9543bd837a
Harold Mesa <harold.mesa@icloud.com>
2023-01-10 00:30:01 -0600

    Add about.js

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+//About page
+import * as React from "react"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About me</h1>
+      <p>I'm good enough, I'm smart enough, and gosh darn it, people like me!</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Page</title>
+
+export default AboutPage
